aaa0907953c4c9ce11441b9862f0a14e6ae459b5
Claudio <cjtejada@outlook.com>
2017-11-21 15:03:44 -0700

    more clean up

--- pids.h
+++ pids.h
@@ -6,7 +6,28 @@
 class PIDs
 {
 public:
-    PIDs();
+    PIDs():
+        MODE01("01"),
+        MODE02("02"),
+        MODE03("03"),
+        MODE04("04"),
+        MODE07("07"),
+        MODE08("08"),
+        MODE09("09"),
+        MODE0A("0A"),
+        Status_Since_DTC_Cleared("01"),
+        FreezeDTC("02"),
+        FuelTankLevel("2F"),
+        EngineLoad("04"),
+        EngineCoolantTemp("05"),
+        FuelPressure("0A"),
+        IntakeManifoldAbsPressure("0B"),
+        RPM("0C"),
+        Speed("0D"),
+        IntakeAirTemp("0F"),
+        ThrottlePosition("11"),
+        AuxStatus("1E")
+        {}
 
     QByteArray getMODE01CurrentData(){
         return MODE01;
